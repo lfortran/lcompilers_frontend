948ecd51b7a21f2bd1fa56e2dc5b261eadfb09eb
Ubaid <shaikhubaid769@gmail.com>
2022-06-23 04:03:31 +0530

    Add all files

---
+++ _next/static/lg-ya8F5vKuMcLrsM9nFQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/32-bbf8ef37dac811ea.js","static/chunks/pages/index-6e4398796b2379bc.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
