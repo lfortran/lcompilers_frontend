57b728cd4d8a6b465e7016baadba502554b66d36
Ubaid <shaikhubaid769@gmail.com>
2022-06-24 01:17:57 +0530

    Wat parsing empty source

--- _next/static/si-bNjrURQ_wL5-FmZsFm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/32-bbf8ef37dac811ea.js","static/chunks/pages/index-a48f96d29db25fa9.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
