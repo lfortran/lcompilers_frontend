7d3f16ac90d3ca35e52e973ff932abfe9f773c47
Ubaid <shaikhubaid769@gmail.com>
2022-08-10 15:36:30 +0530

    Update the lcompilers_frontend url

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const myServer = dev
     ? ""
-    : "/lcompilers_frontend";
+    : "";
 
 // export const imgs = `${server}/imgs`;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: (process.env.NODE_ENV !== "production" ? "" : "/lcompilers_frontend")
+  basePath: (process.env.NODE_ENV !== "production" ? "" : "")
 }
 
 module.exports = nextConfig
