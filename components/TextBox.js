8a257a4cb1dcb72c3dcf705e184728a5d6200d1b
Ubaid <shaikhubaid769@gmail.com>
2022-08-10 18:40:05 +0530

    Use the new Editor component

--- components/TextBox.js
+++ components/TextBox.js
@@ -1,8 +1,11 @@
 // import { useEffect, useRef, useState } from "react";
 import { Button, Tabs, Spin } from "antd";
-import Editor from 'react-simple-code-editor';
 const { TabPane } = Tabs;
 import { PlayCircleOutlined } from "@ant-design/icons";
+import dynamic from 'next/dynamic'
+const Editor = dynamic(import('./Editor'), {
+  ssr: false
+})
 
 function TextBox({ disabled, sourceCode, setSourceCode, activeTab, handleUserTabChange, myHeight }) {
     // const ref = useRef(null);
@@ -20,19 +23,10 @@ function TextBox({ disabled, sourceCode, setSourceCode, activeTab, handleUserTab
     return (
         <div className="card-container" style={{height: "100%" }}>
             <Tabs tabBarExtraContent={extraOperations} style={{ height: "100%" }}>
-                <TabPane tab="main.f90" key="1" style={{ height: myHeight, overflow: "scroll" }}>
+                <TabPane tab="main.f90" key="1" style={{ height: myHeight }}>
                     <Editor
-                        value={sourceCode}
-                        onValueChange={code => setSourceCode(code)}
-                        padding={10}
-                        tabSize={4}
-                        highlight={code => code}
-                        style={{
-                            fontFamily: '"Fira code", "Fira Mono", monospace',
-                            fontSize: 12,
-                            minHeight: "100%",
-                            border: "0.1px solid black"
-                        }}
+                        sourceCode={sourceCode}
+                        setSourceCode={setSourceCode}
                     />
                 </TabPane>
 
