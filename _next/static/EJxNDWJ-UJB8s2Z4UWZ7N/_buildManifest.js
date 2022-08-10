0cf38a22b70e88ba17074be001d11cdf50b3897e
Ubaid <shaikhubaid769@gmail.com>
2022-08-10 21:54:36 +0530

    Update with latest build

---
+++ _next/static/EJxNDWJ-UJB8s2Z4UWZ7N/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/982-2e356aa2e7a17782.js","static/chunks/pages/index-843ff02b47e81d7a.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
