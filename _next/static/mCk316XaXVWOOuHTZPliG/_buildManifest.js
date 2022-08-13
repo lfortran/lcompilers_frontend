d8ae3f2fb8c8e3c15b7c7638ced49891c6c497fe
Deploy <noreply@deploy>
2022-08-13 21:23:49 +0000

    Deploying on 2022-08-13 21:23:49

--- _next/static/mCk316XaXVWOOuHTZPliG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/982-2e356aa2e7a17782.js","static/chunks/pages/index-b14ae636f1864627.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
