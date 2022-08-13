6b55f810533ede8c3cad902ab25fcf4113324463
Ubaid <shaikhubaid769@gmail.com>
2022-08-14 02:27:04 +0530

    Use mandelbrot as default example

--- pages/index.js
+++ pages/index.js
@@ -38,19 +38,89 @@ var lfortran_funcs = {
     execute_code: null,
 };
 
-const initial_source_code = `program expr2
+var src_code_mandel_brot = `program mandelbrot
     implicit none
+    integer  , parameter :: rk       = 8
+    integer  , parameter :: i_max    =  600
+    integer  , parameter :: j_max    =  450
+    integer  , parameter :: n_max    =  100
+    real (rk), parameter :: x_centre = -0.5_rk
+    real (rk), parameter :: y_centre =  0.0_rk
+    real (rk), parameter :: width    =  4.0_rk
+    real (rk), parameter :: height   =  3.0_rk
+    real (rk), parameter :: dx_di    =   width / i_max
+    real (rk), parameter :: dy_dj    = -height / j_max
+    real (rk), parameter :: x_offset = x_centre - 0.5_rk * (i_max + 1) * dx_di
+    real (rk), parameter :: y_offset = y_centre - 0.5_rk * (j_max + 1) * dy_dj
+    integer :: image(i_max, j_max)
+    integer :: image_t(j_max, i_max)
+    integer   :: i
+    integer   :: j
+    integer   :: n
+    real (rk) :: x
+    real (rk) :: y
+    real (rk) :: x_0
+    real (rk) :: y_0
+    real (rk) :: x_sqr
+    real (rk) :: y_sqr
 
-    integer :: x
+    do j = 1, j_max
+    y_0 = y_offset + dy_dj * j
+    do i = 1, i_max
+        x_0 = x_offset + dx_di * i
+        x = 0.0_rk
+        y = 0.0_rk
+        n = 0
+        do
+        x_sqr = x ** 2
+        y_sqr = y ** 2
+        if (x_sqr + y_sqr > 4.0_rk) then
+            image(i,j) = 255
+            exit
+        end if
+        if (n == n_max) then
+            image(i,j) = 0
+            exit
+        end if
+        y = y_0 + 2.0_rk * x * y
+        x = x_0 + x_sqr - y_sqr
+        n = n + 1
+        end do
+    end do
+    end do
 
-    x = (2+3)*5
-    print *, x
+    ! print '(a)', 'P2'
+    ! print '(i0, 1x, i0)', i_max, j_max
+    ! print '(i0)', 255
+    ! do j = 1, 3
+    !   do i = 1, 3
+    !     print '(i0)', image(i,j)
+    !   end do
+    ! end do
 
-end program`;
+    do i = 1, i_max
+        do j = 1, j_max
+            image_t(j, i) = image(i, j)
+        end do
+    end do
+
+    print *, "The result is as follows:"
+    call show_img(j_max, i_max, image_t)
+
+    print *, "Thank you! Hope you had fun!"
+
+    interface
+        subroutine show_img(n, m, A) bind(c)
+        integer, intent(in) :: n, m
+        integer, intent(in) :: A(n,m)
+        end subroutine
+    end interface
+end program mandelbrot
+`;
 
 export default function Home() {
     const [moduleReady, setModuleReady] = useState(false);
-    const [sourceCode, setSourceCode] = useState(initial_source_code);
+    const [sourceCode, setSourceCode] = useState(src_code_mandel_brot);
     const [activeTab, setActiveTab] = useState("STDOUT");
     const [output, setOutput] = useState("");
     const [myHeight, setMyHeight] = useState("calc(100vh - 170px)")
