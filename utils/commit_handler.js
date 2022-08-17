d8890fd77c8c4eac3fbe19962cce8e679cf298dd
Ubaid <shaikhubaid769@gmail.com>
2022-08-17 22:04:44 +0530

    Define getAllCommits() and getCommitFromJson()

---
+++ utils/commit_handler.js
@@ -0,0 +1,18 @@
+import path from 'path'
+import fs from 'fs'
+
+const dataPath = path.join(process.cwd(), 'data.json');
+
+export async function getAllCommits() {
+    const jsonString = fs.readFileSync(dataPath);
+    const commits = JSON.parse(jsonString)["dev"];
+    return commits;
+}
+
+export async function getCommitFromJson(commitId) {
+    const commits = await getAllCommits();
+    var reqCommit = commits.find((currentCommmit)=> {
+        return currentCommmit.commit === commitId
+    });
+    return reqCommit;
+}
