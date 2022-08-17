d8492067a3e2d75b5410199273e126f48de10137
Ubaid <shaikhubaid769@gmail.com>
2022-08-17 23:48:07 +0530

    Add commits submenu to MyHeader

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -1,20 +1,38 @@
-import { HomeOutlined } from '@ant-design/icons';
+import { HomeOutlined, FieldTimeOutlined} from '@ant-design/icons';
 import { Menu } from 'antd';
 import { useState } from "react";
 import HeadMeta from './HeadMeta';
+import { useRouter } from 'next/router';
 
-const items = [
-    {
-        label: <><HomeOutlined /> LFortran</>,
-        key: "1"
-    }
-]
-function MyHeader() {
-    const [current, setCurrent] = useState("1")
+
+function MyHeader({ commits }) {
+    const router = useRouter();
+    const [currentMenuKey, setCurrentMenuKey] = useState("home")
+    const items = [
+        {
+            label: "LFortran",
+            key: "home",
+            icon: <HomeOutlined />,
+            onClick: () => { router.push("/").then(() => router.reload()); }
+        },
+        {
+            label: `Commits`,
+            key: 'commits',
+            children: commits.map((commit) => {
+                return {
+                    label: commit.date,
+                    key: commit.commit,
+                    onClick: () => { router.push(`/commits/${commit.commit}`).then(() => router.reload()) }
+                }
+            }),
+            icon: <><FieldTimeOutlined /> {commits.length}</>,
+        }
+    ]
+
     return (
         <>
             <HeadMeta />
-            <Menu theme='dark' selectedKeys={[current]} mode="horizontal" items={items} />
+            <Menu theme='dark' selectedKeys={[currentMenuKey]} mode="horizontal" items={items} />
         </>
     );
 }
