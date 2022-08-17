5e7136b37566011fad090300bc357daf6c497a74
Deploy <noreply@deploy>
2022-08-17 14:29:14 +0000

    Deploying on 2022-08-17 14:29:14

---
+++ _next/static/0ncc61Mhdgkaj9s82LGzF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/459-debbd8ac024e4827.js","static/chunks/pages/index-f120c9f5059d3c53.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
