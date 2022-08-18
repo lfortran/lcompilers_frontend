f3bede3e14b5012da81224ae8039c047dbb121fc
Ondřej Čertík <ondrej@certik.us>
2022-08-18 11:09:17 +0200

    Plot both color and grayscale

--- utils/source_code_examples.js
+++ utils/source_code_examples.js
@@ -3,7 +3,7 @@ var src_code_mandel_brot = `program mandelbrot
     integer  , parameter :: rk       = 8
     integer  , parameter :: i_max    =  600
     integer  , parameter :: j_max    =  450
-    integer  , parameter :: n_max    =  100
+    integer  , parameter :: n_max    =  255
     real (rk), parameter :: x_centre = -0.5_rk
     real (rk), parameter :: y_centre =  0.0_rk
     real (rk), parameter :: width    =  4.0_rk
@@ -12,7 +12,7 @@ var src_code_mandel_brot = `program mandelbrot
     real (rk), parameter :: dy_dj    = -height / j_max
     real (rk), parameter :: x_offset = x_centre - 0.5_rk * (i_max + 1) * dx_di
     real (rk), parameter :: y_offset = y_centre - 0.5_rk * (j_max + 1) * dy_dj
-    integer :: image(j_max, i_max, 4)
+    integer :: image(j_max, i_max), image_color(j_max, i_max, 4)
     integer   :: i
     integer   :: j
     integer   :: n
@@ -23,6 +23,11 @@ var src_code_mandel_brot = `program mandelbrot
     real (rk) :: x_sqr
     real (rk) :: y_sqr
     interface
+        subroutine show_img(n, m, A) bind(c)
+        integer, intent(in) :: n, m
+        integer, intent(in) :: A(n,m)
+        end subroutine
+
         subroutine show_img_color(n, m, A) bind(c)
         integer, intent(in) :: n, m
         integer, intent(in) :: A(n,m,4)
@@ -36,31 +41,31 @@ var src_code_mandel_brot = `program mandelbrot
             y = 0.0_rk
             n = 0
             do
-            x_sqr = x ** 2
-            y_sqr = y ** 2
-            if (x_sqr + y_sqr > 4.0_rk) then
-                image(j,i,1) = 255
-                image(j,i,2) = 0
-                image(j,i,3) = 0
-                image(j,i,4) = 255
-                exit
-            end if
-            if (n == n_max) then
-                image(j,i,1) = 0
-                image(j,i,2) = 0
-                image(j,i,3) = 255
-                image(j,i,4) = 255
-                exit
-            end if
-            y = y_0 + 2.0_rk * x * y
-            x = x_0 + x_sqr - y_sqr
-            n = n + 1
+                x_sqr = x ** 2
+                y_sqr = y ** 2
+                if (x_sqr + y_sqr > 4.0_rk .or. n == n_max) then
+                    image(j,i) = 255-n
+                    exit
+                end if
+                y = y_0 + 2.0_rk * x * y
+                x = x_0 + x_sqr - y_sqr
+                n = n + 1
             end do
         end do
     end do
-    print *, "The Mandelbrot image is:"
-    call show_img_color(j_max, i_max, image)
-    print *, "Thank you! Hope you had fun!"
+    do j = 1, j_max
+        do i = 1, i_max
+            image_color(j,i,1) = image(j,i)         ! Red
+            image_color(j,i,2) = image(j,i)         ! Green
+            image_color(j,i,3) = 255-image(j,i)/2   ! Blue
+            image_color(j,i,4) = 255                ! Alpha
+        end do
+    end do
+    print *, "The Mandelbrot image in color:"
+    call show_img_color(j_max, i_max, image_color)
+    print *, "The Mandelbrot image in grayscale:"
+    call show_img(j_max, i_max, image)
+    print *, "Done."
 end program mandelbrot
 `;
 
