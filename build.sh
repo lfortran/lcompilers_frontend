84e29b0cbc8adc27bcadfe467e7dcc9d26a93545
Ubaid <shaikhubaid769@gmail.com>
2022-08-18 17:10:05 +0530

    Update build.sh to download files from wasm_builds

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+# curl https://lfortran.github.io/wasm_builds/data.json -o data.json
+latest_commit=`curl https://lfortran.github.io/wasm_builds/latest_commit`
+curl "https://lfortran.github.io/wasm_builds/dev/$latest_commit/lfortran.js" -o public/lfortran.js
+curl "https://lfortran.github.io/wasm_builds/dev/$latest_commit/lfortran.wasm" -o public/lfortran.wasm
+
 npm run build
 npm run export
 echo "dev.lfortran.org" >> out/CNAME
