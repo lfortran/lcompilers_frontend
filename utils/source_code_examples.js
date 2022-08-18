25733f5b7ef5b56414821705b1f00b3296f1c482
Ondřej Čertík <ondrej@certik.us>
2022-08-18 12:21:32 +0200

    Update the code to use row-major order
    Requires https://github.com/lfortran/lfortran/pull/621.

--- utils/source_code_examples.js
+++ utils/source_code_examples.js
@@ -3,16 +3,16 @@ integer, parameter :: Nx = 600, Ny = 450, n_max = 255, dp=8
 real(dp), parameter :: xcenter = -0.5_dp, ycenter = 0.0_dp, &
     width = 4, height = 3, dx_di = width/Nx, dy_dj = -height/Ny, &
     x_offset = xcenter - (Nx+1)*dx_di/2, y_offset = ycenter - (Ny+1)*dy_dj/2
-integer :: image(Ny,Nx), image_color(Ny,Nx,4), palette(4,3), i, j, n, idx
+integer :: image(Nx,Ny), image_color(4,Nx,Ny), palette(3,4), i, j, n, idx
 real(dp) :: x, y, x_0, y_0, x_sqr, y_sqr
 interface
-    subroutine show_img(n, m, A) bind(c)
-    integer, intent(in) :: n, m
-    integer, intent(in) :: A(n,m)
+    subroutine show_img(w, h, A) bind(c)
+    integer, intent(in) :: w, h
+    integer, intent(in) :: A(w,h)
     end subroutine
-    subroutine show_img_color(n, m, A) bind(c)
-    integer, intent(in) :: n, m
-    integer, intent(in) :: A(n,m,4)
+    subroutine show_img_color(w, h, A) bind(c)
+    integer, intent(in) :: w, h
+    integer, intent(in) :: A(4,w,h)
     end subroutine
 end interface
 do j = 1, Ny
@@ -23,7 +23,7 @@ do j = 1, Ny
         do
             x_sqr = x ** 2; y_sqr = y ** 2
             if (x_sqr + y_sqr > 4 .or. n == n_max) then
-                image(j,i) = 255-n
+                image(i,j) = 255-n
                 exit
             end if
             y = y_0 + 2 * x * y
@@ -32,23 +32,23 @@ do j = 1, Ny
         end do
     end do
 end do
-palette(1,1) =   0; palette(1,2) = 135; palette(1,3) =  68
-palette(2,1) =   0; palette(2,2) =  87; palette(2,3) = 231
-palette(3,1) = 214; palette(3,2) =  45; palette(3,3) =  32
-palette(4,1) = 255; palette(4,2) = 167; palette(4,3) =   0
+palette(1,1) =   0; palette(2,1) = 135; palette(3,1) =  68
+palette(1,2) =   0; palette(2,2) =  87; palette(3,2) = 231
+palette(1,3) = 214; palette(2,3) =  45; palette(3,3) =  32
+palette(1,4) = 255; palette(2,4) = 167; palette(3,4) =   0
 do j = 1, Ny
     do i = 1, Nx
-        idx = image(j,i) - (image(j,i)/4)*4 + 1
-        image_color(j,i,1) = palette(idx,1) ! Red
-        image_color(j,i,2) = palette(idx,2) ! Green
-        image_color(j,i,3) = palette(idx,3) ! Blue
-        image_color(j,i,4) = 255            ! Alpha
+        idx = image(i,j) - (image(i,j)/4)*4 + 1
+        image_color(1,i,j) = palette(1,idx) ! Red
+        image_color(2,i,j) = palette(2,idx) ! Green
+        image_color(3,i,j) = palette(3,idx) ! Blue
+        image_color(4,i,j) = 255            ! Alpha
     end do
 end do
 print *, "The Mandelbrot image in color:"
-call show_img_color(Ny, Nx, image_color)
+call show_img_color(Nx, Ny, image_color)
 print *, "The Mandelbrot image in grayscale:"
-call show_img(Ny, Nx, image)
+call show_img(Nx, Ny, image)
 print *, "Done."
 end program mandelbrot
 `;
