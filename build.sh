92a0229fdb2b460906bfeefcf963ae629a1c473b
Ubaid <shaikhubaid769@gmail.com>
2022-08-18 17:25:07 +0530

    Fix url for fetching latest_commit

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # curl https://lfortran.github.io/wasm_builds/data.json -o data.json
-latest_commit=`curl https://lfortran.github.io/wasm_builds/latest_commit`
+latest_commit=`curl https://lfortran.github.io/wasm_builds/dev/latest_commit`
 curl "https://lfortran.github.io/wasm_builds/dev/$latest_commit/lfortran.js" -o public/lfortran.js
 curl "https://lfortran.github.io/wasm_builds/dev/$latest_commit/lfortran.wasm" -o public/lfortran.wasm
 
