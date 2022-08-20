34d4bf238c196e7a2f5eb236e420194aa7c33565
Deploy <noreply@deploy>
2022-08-20 13:34:04 +0000

    Deploying on 2022-08-20 13:34:04

---
+++ .next/static/AwywupUtQrpGX8N9ecwBe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/459-debbd8ac024e4827.js","static/chunks/pages/index-b60e18f0484b8f0b.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
