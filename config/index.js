0e80d775b6edc28d68355cec526c6ea60cbdcc7e
Ubaid <shaikhubaid769@gmail.com>
2022-08-22 03:13:50 +0530

    Fix path

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const myServer = dev
     ? ""
-    : "";
+    : "/pull_request_preview";
 
 // export const imgs = `${server}/imgs`;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: (process.env.NODE_ENV !== "production" ? "" : "")
+  basePath: (process.env.NODE_ENV !== "production" ? "" : "/pull_request_preview")
 }
 
 module.exports = nextConfig
