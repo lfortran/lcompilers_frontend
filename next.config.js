e881b2056e5c22e69629f19de6442fcbbb423dd9
Ubaid <shaikhubaid769@gmail.com>
2022-08-22 14:49:18 +0530

    Revert "Fix path"
    This reverts commit 0e80d775b6edc28d68355cec526c6ea60cbdcc7e.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: (process.env.NODE_ENV !== "production" ? "" : "/pull_request_preview")
+  basePath: (process.env.NODE_ENV !== "production" ? "" : "")
 }
 
 module.exports = nextConfig
