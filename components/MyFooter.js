6189703f8e56fddc0fe1d49863f9eba227f0678e
Bhaswati Roy <roybhaswati24@gmail.com>
2023-01-16 19:26:54 +0530

    Pipe character added to improve readability

--- components/MyFooter.js
+++ components/MyFooter.js
@@ -12,10 +12,10 @@ function MyFooter() {
                 width: "100%",
             }}
         >
-            <a href="https://lfortran.org/">LFortran</a> (<a href={"https://github.com/lfortran/lfortran/commit/" + commit.id}>{commit.id}</a>) by 
-            <a href="https://lcompilers.org/">LCompilers</a> 
-            <a href="https://github.com/lfortran/lcompilers_frontend">Contribute</a>
-            <a href="https://github.com/lfortran/lcompilers_frontend/issues">Report Issues</a>
+            <a href="https://lfortran.org/">LFortran</a> (<a href={"https://github.com/lfortran/lfortran/commit/" + commit.id}>{commit.id}</a>) by &nbsp;
++            <a href="https://lcompilers.org/">LCompilers</a> | &nbsp;
++            <a href="https://github.com/lfortran/lcompilers_frontend">Contribute</a> |&nbsp;
++            <a href="https://github.com/lfortran/lcompilers_frontend/issues">Report Issues</a>
         </Footer>
     );
 }
