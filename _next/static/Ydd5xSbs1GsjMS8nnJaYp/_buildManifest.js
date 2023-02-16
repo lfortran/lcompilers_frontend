abcb2975e779fae17718192d0779263bb1c87ffe
Deploy <noreply@deploy>
2023-02-16 04:07:00 +0000

    Deploying on 2023-02-16 04:06:59

---
+++ _next/static/Ydd5xSbs1GsjMS8nnJaYp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/637-750382410f547eb3.js","static/chunks/pages/index-8ba9434f0da3c62c.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/lzBR3YwKdoXwLQ35sb-Sq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/637-750382410f547eb3.js","static/chunks/pages/index-db771dc8d56b5356.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
