79c5492c5b5a76f73e1604d7ad6b29be113f8f6d
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-03-06 22:57:31 +0530

    Remove dependency on MY_ENV for local development

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: (process.env.MY_ENV !== "production" ? "/pull_request_preview" : "")
+  basePath: (process.env.MY_ENV === "development" ? "/pull_request_preview" : "")
 }
 
 module.exports = nextConfig
