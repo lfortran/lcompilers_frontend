87333ae59c9860324cf625bc3fa97e57f8158730
Deploy <noreply@deploy>
2023-08-28 23:47:05 +0000

    Deploying on 2023-08-28 23:47:04

---
+++ _next/static/joPsNCr-PRR7KFolB_Pil/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/20-d8526c370ef1aacd.js","static/chunks/pages/index-60d4e58131f58235.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
