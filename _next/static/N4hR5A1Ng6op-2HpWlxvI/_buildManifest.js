d5fc5ba59853c20a46152f45504331e875a4d7fc
Deploy <noreply@deploy>
2026-02-21 01:35:06 +0000

    Deploying on 2026-02-21 01:35:05

--- _next/static/N4hR5A1Ng6op-2HpWlxvI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/334-8ce1b7cdb3dc2e95.js","static/chunks/pages/index-2667a0b69e467d6c.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/krtvT01z-7n8NOvjPE1yx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/43-19905735f7a962d9.js","static/chunks/pages/index-955625d1256d73eb.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
